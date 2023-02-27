df4991564d24331945601dd1f087a83b8b1f8f36
Carla-Daniela <daninevesdn22@gmail.com>
2023-02-27 19:51:24 +0000

    Version-1.0: adicionando o user-data

---
+++ myuserdata.sh
@@ -0,0 +1,8 @@
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
+
